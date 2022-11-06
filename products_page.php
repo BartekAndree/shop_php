99556b8ae30bc793a0cd1aa05a4976a855a9f991
Bartek Andree <73586879+BartekAndree@users.noreply.github.com>
2022-11-06 23:53:33 +0100

    Add files via upload

---
+++ products_page.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BestToysEver</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.reflowhq.com/v2/toolkit.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
+</head>
+
+<body>
+    <?php include 'navbar.php'; ?>
+
+    <body>
+
+        <section class="py-5">
+            <div class="container py-5">
+                <div class="row mb-4 mb-lg-5">
+                    <div class="col-md-8 col-xl-6 text-center mx-auto">
+                        <h2 class="fw-bold">Products</h2>
+                        <p class="text-muted w-lg-50">No matter the project, our team can handle it.</p>
+                    </div>
+                </div>
+
+                <div class="row row-cols-2 row-cols-md-3 mx-auto" style="max-width: 900px;">
+                    <div class="col mb-4">
+                        <div class="text-center"><img class="rounded mb-3 fit-cover" width="200" height="200" src="assets/img/products/1.jpg">
+                            <h5 class="fw-bold mb-0"><strong>Ananas</strong></h5>
+                            <p class="text-muted mb-2">Erat netus</p>
+                            <a class="btn btn-primary shadow" role="button" href="">ZAMÓW</a>
+                        </div>
+                    </div>
+                    <div class="col mb-4">
+                        <div class="text-center"><img class="rounded mb-3 fit-cover" width="200" height="200" src="assets/img/products/2.jpg">
+                            <h5 class="fw-bold mb-0"><strong>Wiśnia</strong></h5>
+                            <p class="text-muted mb-2">Erat netus</p>
+                            <a class="btn btn-primary shadow" role="button" href="">ZAMÓW</a>
+                        </div>
+                    </div>
+                    <div class="col mb-4">
+                        <div class="text-center"><img class="rounded mb-3 fit-cover" width="200" height="200" src="assets/img/products/3.jpg">
+                            <h5 class="fw-bold mb-0"><strong>Kaktus</strong></h5>
+                            <p class="text-muted mb-2">Erat netus</p>
+                            <a class="btn btn-primary shadow" role="button" href="">ZAMÓW</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+    </body>
+
+
+    <?php include 'footer.php'; ?>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="assets/js/bold-and-bright.js"></script>
+</body>
+
+</html>
---
+++ testimonials.php
@@ -0,0 +1,46 @@
+<section class="py-5 mt-3">
+    <div class="container py-5">
+        <div class="row mb-5">
+            <div class="col-md-8 col-xl-6 text-center mx-auto">
+                <p class="fw-bold text-success mb-2">Testimonials</p>
+                <h2 class="fw-bold"><strong>What People Say About us</strong></h2>
+                <p class="text-muted w-lg-50">No matter the project, our team can handle it.&nbsp;</p>
+            </div>
+        </div>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 d-sm-flex justify-content-sm-center">
+            <div class="col mb-4">
+                <div class="d-flex flex-column align-items-center align-items-sm-start">
+                    <p class="bg-light border rounded border-light p-4">Nisi sit justo faucibus nec ornare amet, tortor torquent. Blandit class dapibus, aliquet morbi.</p>
+                    <div class="d-flex"><img class="rounded-circle flex-shrink-0 me-3 fit-cover" width="50" height="50" src="assets/img/avatars/avatar2.jpg">
+                        <div>
+                            <p class="fw-bold text-primary mb-0">Wojtek</p>
+                            <p class="text-muted mb-0">Erat netus</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col mb-4">
+                <div class="d-flex flex-column align-items-center align-items-sm-start">
+                    <p class="bg-light border rounded border-light p-4">Nisi sit justo faucibus nec ornare amet, tortor torquent. Blandit class dapibus, aliquet morbi.</p>
+                    <div class="d-flex"><img class="rounded-circle flex-shrink-0 me-3 fit-cover" width="50" height="50" src="assets/img/avatars/avatar1.jpg">
+                        <div>
+                            <p class="fw-bold text-primary mb-0">Bartek</p>
+                            <p class="text-muted mb-0">Erat netus</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col mb-4">
+                <div class="d-flex flex-column align-items-center align-items-sm-start">
+                    <p class="bg-light border rounded border-light p-4">Nisi sit justo faucibus nec ornare amet, tortor torquent. Blandit class dapibus, aliquet morbi.</p>
+                    <div class="d-flex"><img class="rounded-circle flex-shrink-0 me-3 fit-cover" width="50" height="50" src="assets/img/avatars/avatar3.jpg">
+                        <div>
+                            <p class="fw-bold text-primary mb-0">Marcin</p>
+                            <p class="text-muted mb-0">Erat netus</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
